9a0ce329bdf39155c6f3c7a5499f19fc9909c031
Dokho <palbabyshark@gmail.com>
2023-11-27 11:36:11 +0000

    app bar in progress + backg image

--- src/Layout/HomeRoot.jsx
+++ src/Layout/HomeRoot.jsx
@@ -1,14 +1,16 @@
-import { Outlet } from "react-router-dom"
+import { Outlet } from "react-router-dom";
+import Header from "../Components/Header/Header";
+import Footer from "../Components/Footer/Footer";
+import "../Components/Home/Home.css"
 
-function HomeRoot () {
-    return (
-        <>
-        <div>header</div>
-        <Outlet/>
-        <div>footer</div>
-        </>
-        
-    )
+function HomeRoot() {
+  return (
+    <div id="background">
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 }
 
-export default HomeRoot
+export default HomeRoot;
