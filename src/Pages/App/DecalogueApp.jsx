51529c3114c006335b66b382f9012809b1b806e7
caarolbrown <carolinamorenofdez@gmail.com>
2023-12-01 13:13:46 +0000

    decalogueApp cambiada la letra

--- src/Pages/App/DecalogueApp.jsx
+++ src/Pages/App/DecalogueApp.jsx
@@ -5,30 +5,35 @@ function DecalogueApp() {
   return (
     <Box
       sx={{
+        boxShadow: "10px 10px 28px -7px rgba(114, 9, 183, 1)",
         padding: "2rem",
         maxWidth: "800px",
         margin: "0 auto",
         textAlign: "center",
-        bgcolor: "rgba(114, 9, 183, 0.2 )",
+        bgcolor: "white",
         color: "white",
         borderRadius: 2,
         border: "3px solid white",
       }}
     >
-      <Typography variant="h3" component="h1" gutterBottom>
-        Decalogue for Kids
+      <Typography variant="h3" component="h1" gutterBottom
+      sx={{
+        marginTop: "25px"
+      }}>
+        <b style={{ fontSize: "50px", color: "#d6bbe9"}}>Decalogue for Kids</b>
       </Typography>
       <Typography variant="h5" sx={{ mt: 2, mb: 2 }}>
-        Navigating Social Media with Care and Respect
+        <b style={{fontSize: "35px", color: "#d6bbe9"}}>Navigating Social Media with Care and Respect</b>
       </Typography>
       <List>
         <ListItem>
           <ListItemText
             primary={
               <>
-                <b style={{fontSize: "20px"}}>1. Be Kind Online</b>
-                <br /> Treat others as you would like to be treated. Kindness is
-                key.
+                <b style={{fontSize: "25px", color: "black"}}>1. Be Kind Online</b>
+                <br></br>
+                <b style={{fontSize: "20px", color: "#d6bbe9"}}>Treat others as you would like to be treated. Kindness is
+                key.</b>
               </>
             }
           />
@@ -37,8 +42,9 @@ function DecalogueApp() {
           <ListItemText
             primary={
               <>
-                <b style={{fontSize: "20px"}}>2. Privacy is Precious</b>
-                <br /> Keep your personal information private and safe.
+                <b style={{fontSize: "25px", color: "black"}}>2. Privacy is Precious</b>
+                <br></br>
+                <b style={{fontSize: "20px", color: "#d6bbe9"}}>Keep your personal information private and safe.</b>
               </>
             }
           />
@@ -47,8 +53,9 @@ function DecalogueApp() {
           <ListItemText
             primary={
               <>
-                <b style={{fontSize: "20px"}}>3. Pause Before You Post</b>
-                <br /> Think about the impact of your words and actions online.
+                <b style={{fontSize: "25px", color: "black"}}>3. Pause Before You Post</b>
+                <br></br>
+                <b style={{fontSize: "20px", color: "#d6bbe9"}}>Think about the impact of your words and actions online.</b>
               </>
             }
           />
@@ -57,9 +64,9 @@ function DecalogueApp() {
           <ListItemText
             primary={
               <>
-                <b style={{fontSize: "20px"}}>4. Respect Boundaries</b>
-                <br /> Ask for permission before sharing others' information or
-                photos.
+                <b style={{fontSize: "25px", color: "black"}}>4. Respect Boundaries</b>
+                <br></br>
+                <b style={{fontSize: "20px", color: "#d6bbe9"}}>Ask for permission before sharing others' information or photos.</b>
               </>
             }
           />
@@ -68,9 +75,9 @@ function DecalogueApp() {
           <ListItemText
             primary={
               <>
-                <b style={{fontSize: "20px"}}>5. Balance Your Time</b>
-                <br /> Make time for offline activities and enjoy the world
-                outside screens.
+                <b style={{fontSize: "25px", color: "black"}}>5. Balance Your Time</b>
+                <br></br>
+                <b style={{fontSize: "20px", color: "#d6bbe9"}}>Make time for offline activities and enjoy the world outside screens.</b>
               </>
             }
           />
@@ -79,8 +86,9 @@ function DecalogueApp() {
           <ListItemText
             primary={
               <>
-                <b style={{fontSize: "20px"}}>6. Speak Up About Bullying</b>
-                <br /> If you witness bullying, tell a trusted adult.
+                <b style={{fontSize: "25px", color: "black"}}>6. Speak Up About Bullying</b>
+                <br></br>
+                <b style={{fontSize: "20px", color: "#d6bbe9"}}>If you witness bullying, tell a trusted adult.</b>
               </>
             }
           />
@@ -89,9 +97,9 @@ function DecalogueApp() {
           <ListItemText
             primary={
               <>
-                <b style={{fontSize: "20px"}}>7. Be a Critical Thinker</b>
-                <br /> Don't believe everything you see online; research and
-                question.
+                <b style={{fontSize: "25px", color: "black"}}>7. Be a Critical Thinker</b>
+                <br></br>
+                <b style={{fontSize: "20px", color: "#d6bbe9"}}>Don't believe everything you see online; research and question.</b> 
               </>
             }
           />
@@ -100,8 +108,9 @@ function DecalogueApp() {
           <ListItemText
             primary={
               <>
-                <b style={{fontSize: "20px"}}>8. Positive Vibes Only</b>
-                <br /> Use social media to spread positivity and good vibes.
+                <b style={{fontSize: "25px", color: "black"}}>8. Positive Vibes Only</b>
+                <br></br>
+                <b style={{fontSize: "20px", color: "#d6bbe9"}}> Use social media to spread positivity and good vibes.</b>
               </>
             }
           />
@@ -110,9 +119,9 @@ function DecalogueApp() {
           <ListItemText
             primary={
               <>
-                <b style={{fontSize: "20px"}}>9. Take a Break</b>
-                <br /> If social media feels overwhelming, it's okay to step
-                back.
+                <b style={{fontSize: "25px", color: "black"}}>9. Take a Break</b>
+                <br></br>
+                <b style={{fontSize: "20px", color: "#d6bbe9"}}> If social media feels overwhelming, it's okay to step back.</b>
               </>
             }
           />
@@ -121,9 +130,9 @@ function DecalogueApp() {
           <ListItemText
             primary={
               <>
-                <b style={{fontSize: "20px"}}>10. Ask for Help</b>
-                <br /> If something online makes you uncomfortable, talk to an
-                adult.
+                <b style={{fontSize: "25px", color: "black"}}>10. Ask for Help</b>
+                <br></br>
+                <b style={{fontSize: "20px", color: "#d6bbe9"}}> If something online makes you uncomfortable, talk to an adult.</b>
               </>
             }
           />
