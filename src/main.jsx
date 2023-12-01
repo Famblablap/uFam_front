c694d5e54922e7aef71f1f8af2a11c7a2548d698
caarolbrown <carolinamorenofdez@gmail.com>
2023-12-01 09:46:54 +0000

    cambio en el tamaño de las letras en support

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { customTheme } from './assets/context/context.jsx'
+import { ThemeProvider } from '@emotion/react'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ThemeProvider theme={customTheme}>
     <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
+ 
