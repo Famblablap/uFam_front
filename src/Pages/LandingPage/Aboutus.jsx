b3e876032deddad7ed5238a9c335a9e377ea5540
Dokho <palbabyshark@gmail.com>
2023-12-01 14:48:37 +0000

    main2f merged

--- src/Pages/LandingPage/Aboutus.jsx
+++ src/Pages/LandingPage/Aboutus.jsx
@@ -40,43 +40,59 @@ function AboutUs() {
         >
           Connecting Families, Building Bonds
         </Typography>
-        <Typography variant="body1" paragraph style={{fontSize: "15px"}}>
-          At <b style={{fontSize: "18px", color: "#5a5a84"}}>uFam</b>, we believe that the heart of a family can extend into
-          the digital world. Our platform is designed to be a safe and private
-          space where families can come together to share moments, memories, and
+        <Typography variant="body1" paragraph style={{ fontSize: "15px" }}>
+          At <b style={{ fontSize: "20px", fontWeight: "bold" }}>uFam</b>, we
+          believe that the heart of a family can extend into the digital world.
+          Our platform is designed to be a{" "}
+          <b style={{ fontSize: "18px", color: "#5a5a84" }}>
+            safe and private space{" "}
+          </b>
+          where families can come together to share moments, memories, and
           messages without the worry of external influences or privacy concerns.
         </Typography>
-        <Typography variant="body1" paragraph style={{fontSize: "15px"}}>
+        <Typography variant="body1" paragraph style={{ fontSize: "15px" }}>
           Our journey began with a simple vision: to create a social media
           environment that nurtures positive interactions, fosters healthy
-          digital habits, and respects the user's time and privacy. <b style={{fontSize: "18px", color: "#5a5a84"}}>uFam</b>{" "}
-          is more than just an app; it's a community, a space where the
-          connection is genuine, the experiences are real, and the memories last
-          a lifetime.
+          digital habits, and respects the user's time and privacy.
+          <b style={{ fontSize: "20px", fontWeight: "bold" }}> uFam</b> is more
+          than just an app;{" "}
+          <b style={{ fontSize: "18px", color: "#5a5a84" }}>it's a community</b>
+          , a space where the connection is genuine, the experiences are real,
+          and the memories last a lifetime.
         </Typography>
-        <Typography variant="body1" paragraph style={{fontSize: "15px"}}>
-          As parents and caregivers, we understand the challenge of introducing
-          the digital world to the younger generation. That's why we've built
-          <b style={{fontSize: "18px", color: "#5a5a84"}}>uFam</b> to be a learning platform as much as it is a social one,
-          guiding children and teenagers through the nuances of online
-          communication with a focus on digital literacy and citizenship.
+        <Typography variant="body1" paragraph style={{ fontSize: "15px" }}>
+          As parents and caregivers,{" "}
+          <b style={{ fontSize: "18px", color: "#5a5a84" }}>
+            we understand the challenge of introducing the digital world to the
+            younger generation.{" "}
+          </b>
+          That's why we've built
+          <b style={{ fontSize: "20px", fontWeight: "bold" }}> uFam</b> to be a
+          learning platform as much as it is a social one, guiding children and
+          teenagers through the nuances of online communication with a focus on
+          digital literacy and citizenship.
         </Typography>
-        <Typography variant="body1" paragraph style={{fontSize: "15px"}}>
+        <Typography variant="body1" paragraph style={{ fontSize: "15px" }}>
           Our commitment is to continuously evolve with the needs of our
           families. We dedicate ourselves to creating features that enhance
           communication, celebrate milestones, and safeguard the wellbeing of
           all our members. From our family to yours, we welcome you to{" "}
-          <b style={{fontSize: "18px", color: "#5a5a84"}}>uFam</b> - where family comes first, always.
+          <b style={{ fontSize: "20px", fontWeight: "bold" }}>uFam</b>
+          <b style={{ fontSize: "18px", color: "#5a5a84" }}>
+            {" "}
+            - where family comes first, always.
+          </b>
         </Typography>
-        <Typography variant="body1" paragraph style={{fontSize: "15px"}}>
+        <Typography variant="body1" paragraph style={{ fontSize: "15px" }}>
           Join us on this journey and be a part of a growing movement that's
-          reshaping the way families interact online. Welcome to the <b style={{fontSize: "18px", color: "#5a5a84"}}>uFam </b>
+          reshaping the way families interact online. Welcome to the{" "}
+          <b style={{ fontSize: "20px", fontWeight: "bold" }}>uFam </b>
           community – where every family member has a place, a voice, and a
           chance to thrive in the digital age.
         </Typography>
       </Box>
     </Container>
-  )
+  );
 }
 
-export default AboutUs
+export default AboutUs;
--- src/Pages/LandingPage/Faqweb.jsx
+++ src/Pages/LandingPage/Faqweb.jsx
@@ -22,48 +22,49 @@ const data = {
       title: "What do you call an elephant that doesn't matter?",
       content: "Irrelephant lol",
     },
-      {
-        title: "What do you call a fake noodle?",
-        content: "An impasta.",
-      },
-      {
-        title: "How do you make a tissue dance?",
-        content: "Put a little boogie in it.",
-      },
-      {
-        title: "Why did the bicycle fall over?",
-        content: "Because it was two-tired!",
-      },
-      {
-        title: "What do you call cheese that isn't yours?",
-        content: "Nacho cheese.",
-      },
-      {
-        title: "What do you get when you cross a snowman and a vampire?",
-        content: "Frostbite.",
-      },
-      {
-        title: "Why don't skeletons fight each other?",
-        content: "They don't have the guts.",
-      },
-      {
-        title: "What did the janitor say when he jumped out of the closet?",
-        content: "Supplies!",
-      },
-      {
-        title: "What's orange and sounds like a parrot?",
-        content: "A carrot.",
-      },
-      {
-        title: "Why did the golfer bring two pairs of pants?",
-        content: "In case he got a hole in one.",
-      },
+    {
+      title: "What do you call a fake noodle?",
+      content: "An impasta.",
+    },
+    {
+      title: "How do you make a tissue dance?",
+      content: "Put a little boogie in it.",
+    },
+    {
+      title: "Why did the bicycle fall over?",
+      content: "Because it was two-tired!",
+    },
+    {
+      title: "What do you call cheese that isn't yours?",
+      content: "Nacho cheese.",
+    },
+    {
+      title: "What do you get when you cross a snowman and a vampire?",
+      content: "Frostbite.",
+    },
+    {
+      title: "Why don't skeletons fight each other?",
+      content: "They don't have the guts.",
+    },
+    {
+      title: "What did the janitor say when he jumped out of the closet?",
+      content: "Supplies!",
+    },
+    {
+      title: "What's orange and sounds like a parrot?",
+      content: "A carrot.",
+    },
+    {
+      title: "Why did the golfer bring two pairs of pants?",
+      content: "In case he got a hole in one.",
+    },
   ],
 }
 
+
 function Faqweb() {
   return (
-    <Box sx={{ width: "100%", height: "100vh", mt: 0,  }}>
+    <Box sx={{ width: "100%", height: "100vh", mt: 0 }}>
       <Box
         sx={{
           display: "flex",
@@ -81,10 +82,12 @@ function Faqweb() {
           boxSizing: "border-box",
         }}
       >
-        <Faq data={data} />
+        <Box sx={{ "& .faq-row": { fontSize: "2rem", margin: "10px" } }}>
+          <Faq data={data} />
+        </Box>
       </Box>
     </Box>
-  )
+  );
 }
 
 export default Faqweb
