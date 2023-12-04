cbc8295d30b38526a2048f9b6d1a6468c7dd42e3
TheshiaR <cuentaparaclassroomtheshia@gmail.com>
2023-12-04 09:27:29 +0000

    main

--- src/Services/config.js
+++ src/Services/config.js
@@ -4,4 +4,8 @@ const api = axios.create({
   baseURL: "https://ufam-back.onrender.com/api"
 })
 
+// const api = axios.create({
+//   baseURL: "http://localhost:3000/api"
+// })
+
 export default api
